bf59db61d7220ad374ab61239efcc7325f25dc84
Tufan AKÇAY <132299647+tufanakcay@users.noreply.github.com>
2023-05-18 21:01:53 +0300

    Update Fragment

--- app/src/main/java/com/tufanakcay/javafragment/MainActivity.java
+++ app/src/main/java/com/tufanakcay/javafragment/MainActivity.java
@@ -1,8 +1,11 @@
 package com.tufanakcay.javafragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void goToFirst(View view) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        FirstFragment firstFragment = new FirstFragment();
+        //fragmentTransaction.add(R.id.frame_layout,firstFragment).commit();
+        fragmentTransaction.replace(R.id.frame_layout,firstFragment).commit();
+
+
+    }
+
+    public void goToSecond(View view){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        SecondFragment secondFragment = new SecondFragment();
+        fragmentTransaction.replace(R.id.frame_layout,secondFragment).commit();
+
     }
 }
